f199dbf38be234b3c136f04d471496301feb5318
Bicka <Hrab.costin@gmail.com>
2022-06-16 14:27:42 +0300

    Scope

---
+++ Scope/script.js
@@ -0,0 +1,19 @@
+'use strict';
+function calcAge (bYear){
+    const age = new Date().getFullYear() - bYear;
+
+    function printAge (){
+        const output= `${fName}: ${age}, born in ${bYear}`;
+        console.log(output);
+
+        if(bYear>=1981 && bYear <=1996){
+            const str = `${fName}...`;
+            console.log(str);
+        }
+    }
+    printAge();
+    return age;
+}
+
+const fName = "Marcel";
+calcAge(1996)
